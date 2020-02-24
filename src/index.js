19b8636466f175de422d4404fbf8c6930fc85e9d
aleksey_mu <pacuklex@mail.ru>
2020-02-24 14:06:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let direction = "left-right";
+  const result = [];
+  
+  if (!Array.isArray(matrix)) {
+    return [];
+  }
+
+  for (row of matrix) {
+    if (direction === "left-right") {
+      for (num of row)
+      result.push(num);
+      direction = "right-left";
+    } else {
+      for (let i = row.length - 1; i >= 0; i -= 1) {
+        result.push(row[i]);
+        direction = "left-right";
+      }
+    }
+  }
+  return result;
 }
